d2b80f0f69fd9ec4f79bcac2f8674cf0e1284a3f
Joel Walker <hello@joelwalker.co.uk>
2026-01-31 16:06:21 +0000

    Add player state

--- app/src/scenes/soundSelectScene.ts
+++ app/src/scenes/soundSelectScene.ts
@@ -1,20 +1,25 @@
-import { renderBackground } from "../renderBackground";
-import { controllerImageSrcWidth, drawController } from "../renderControllers";
-import type { BuzzerState } from "../types/buzz";
+import { getPlayerState, updateLedState } from '../player-state';
+import { renderBackground } from '../renderBackground';
+import { controllerImageSrcWidth, drawController } from '../renderControllers';
+import type { BuzzerState } from '../types/buzz';
 
-export const update = (deltaTime: number) => { }
-export const render = (
-    deltaTime: number,
-    buzzState: BuzzerState[],
-    canvas: HTMLCanvasElement,
-    ctx: CanvasRenderingContext2D
+export const update = (deltaTime: number) => {};
+export const render = async (
+  deltaTime: number,
+  buzzState: BuzzerState[],
+  canvas: HTMLCanvasElement,
+  ctx: CanvasRenderingContext2D
 ) => {
-    renderBackground(canvas, ctx);
+  renderBackground(canvas, ctx);
 
-    buzzState.forEach((state, index) => {
-        let xWobble = Math.sin((Date.now() / 1000) + index) * 10;
-        let yWobble = Math.cos((Date.now() / 1000) + index) * 10;
+  buzzState.forEach((state, index) => {
+    getPlayerState(index).hasBuzzed(state.buzz);
+  });
 
-        drawController(xWobble + (90 + index * controllerImageSrcWidth), yWobble + 200, state, ctx);
-    });
-}
+  buzzState.forEach((state, index) => {
+    let xWobble = Math.sin(Date.now() / 1000 + index) * 10;
+    let yWobble = Math.cos(Date.now() / 1000 + index) * 10;
+
+    drawController(xWobble + (90 + index * controllerImageSrcWidth), yWobble + 200, state, ctx);
+  });
+};
